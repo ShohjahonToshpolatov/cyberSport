93fcd06b1ec1764c3ca459636d2b1f69259a8ddb
Shohjahon <shohjahon0192@gmail.com>
2026-06-18 04:56:49 +0500

    feat: setup angular routing for auth page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { AuthComponent } from './components/auth/auth.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'register', pathMatch: 'full' },
+
+    { path: 'register', component: AuthComponent },
+
+    { path: '**', redirectTo: 'register' }
+];
